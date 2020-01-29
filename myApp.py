66f6400e5eba7054ef9f81d583d04ba10c455b8b
Matt Johnson <matjohn2@cisco.com>
2020-01-29 15:24:47 +0100

    Changed for git demo session

--- myApp.py
+++ myApp.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Cisco LIVE! Barcelona"
+    return "Hello from Cisco LIVE! Barcelona git session"
 
 @app.route("/hostname", methods=['GET'])
 def hostname():
